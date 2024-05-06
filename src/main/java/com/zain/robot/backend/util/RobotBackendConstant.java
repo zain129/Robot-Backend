a399ef67e295563f19a670acdf58036ef9b28456
Zain I <zain.imtiaz93@gmail.com>
2024-05-07 00:19:18 +0400

    Include the face position constant LEFT

--- src/main/java/com/zain/robot/backend/util/RobotBackendConstant.java
+++ src/main/java/com/zain/robot/backend/util/RobotBackendConstant.java
@@ -2,6 +2,7 @@ package com.zain.robot.backend.util;
 
 public class RobotBackendConstant {
     public static final String FACE_RIGHT = "RIGHT";
+    public static final String FACE_LEFT = "LEFT";
     public static final String FACE_UP = "UP";
     public static final String FACE_DOWN = "DOWN";
 }
