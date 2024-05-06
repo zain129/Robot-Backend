0dfdcf7de62ea62187c30cca280647dae0f75998
Zain I <zain.imtiaz93@gmail.com>
2024-05-07 00:20:36 +0400

    Updated the RobotBackendController and added a new POST method to get the commands in a single call and send back response after processing all the commands

--- src/main/java/com/zain/robot/backend/controller/RobotBackendController.java
+++ src/main/java/com/zain/robot/backend/controller/RobotBackendController.java
@@ -6,8 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @RequestMapping("/robot-backend-default")
 public interface RobotBackendController {
 
     ResponseEntity<CommandResponseDTO> executeCommands(@RequestBody CommandRequestDTO commandRequestDTO);
+
+    ResponseEntity<List<CommandResponseDTO>> executeAllCommands(@RequestBody List<CommandRequestDTO> commandRequestDTO);
 }
--- src/main/java/com/zain/robot/backend/controller/RobotBackendControllerImpl.java
+++ src/main/java/com/zain/robot/backend/controller/RobotBackendControllerImpl.java
@@ -6,11 +6,13 @@ import com.zain.robot.backend.service.RobotBackendService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Objects;
 
 @RestController
@@ -25,9 +27,19 @@ public class RobotBackendControllerImpl implements RobotBackendController {
     @PostMapping("/execute-commands")
     public ResponseEntity<CommandResponseDTO> executeCommands(@RequestBody CommandRequestDTO commandRequestDTO) {
         log.info("Entering executeCommands controller method to execute command {}", commandRequestDTO);
-        CommandResponseDTO commandResponseDTO = robotBackendService.executeCommands(commandRequestDTO);
+        CommandResponseDTO commandResponseDTO = robotBackendService.executeCommand(commandRequestDTO);
 
         if (Objects.isNull(commandResponseDTO)) return ResponseEntity.badRequest().body(null);
+        return ResponseEntity.ok(commandResponseDTO);
+    }
+
+    @Override
+    @PostMapping("/execute-all-commands")
+    public ResponseEntity<List<CommandResponseDTO>> executeAllCommands(@RequestBody List<CommandRequestDTO> commandRequestDTO) {
+        log.info("Entering executeAllCommands controller method to execute commands {}", commandRequestDTO);
+        List<CommandResponseDTO> commandResponseDTO = robotBackendService.executeAllCommands(commandRequestDTO);
+
+        if (CollectionUtils.isEmpty(commandResponseDTO)) return ResponseEntity.badRequest().body(null);
         return ResponseEntity.ok(commandResponseDTO);
     }
 }
