5aff737a8fa913819dcde2611a6180b6b9221d86
Zain I <zain.imtiaz93@gmail.com>
2024-05-07 01:30:11 +0400

    Removed unused import

--- src/test/java/com/zain/robot/backend/controller/RobotBackendControllerImplTest.java
+++ src/test/java/com/zain/robot/backend/controller/RobotBackendControllerImplTest.java
@@ -21,7 +21,6 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.mockito.Mockito.when;
 
